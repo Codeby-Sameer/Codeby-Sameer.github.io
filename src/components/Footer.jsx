a2794f4d35f2fe58cb3aa222253aa970c2a09b55
Sam <sameershaikzayn@gmail.com>
2025-11-19 22:44:38 +0530

    links updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -41,31 +41,32 @@ const Footer = () => {
           <div>
             <h3 className="text-lg font-semibold mb-4">Follow Us</h3>
             <div className="flex space-x-4">
-              <a href="#" className="text-gray-300 hover:text-white transition-colors">
+<a href="https://www.linkedin.com/company/gc-technologies-pvt-ltd/" target="_blank" className="text-gray-300 hover:text-white transition-colors">
                 <span className="sr-only">LinkedIn</span>
                 <svg className="h-6 w-6" fill="currentColor" viewBox="0 0 24 24">
                   <path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"/>
                 </svg>
               </a>
-              <a href="#" className="text-gray-300 hover:text-white transition-colors">
+<a href="https://www.instagram.com/gctechsoft?igsh=MTlmOGU4NTliaTByNA%3D%3D&utm_source=qr" target="_blank" className="text-gray-300 hover:text-white transition-colors">
   <span className="sr-only">Instagram</span>
   <svg className="h-6 w-6" fill="currentColor" viewBox="0 0 24 24">
     <path d="M7.75 2h8.5A5.75 5.75 0 0122 7.75v8.5A5.75 5.75 0 0116.25 22h-8.5A5.75 5.75 0 012 16.25v-8.5A5.75 5.75 0 017.75 2zm0 1.5A4.25 4.25 0 003.5 7.75v8.5A4.25 4.25 0 007.75 20.5h8.5a4.25 4.25 0 004.25-4.25v-8.5A4.25 4.25 0 0016.25 3.5h-8.5zM12 7a5 5 0 110 10 5 5 0 010-10zm0 1.5a3.5 3.5 0 100 7 3.5 3.5 0 000-7zm5.25-.75a.75.75 0 110 1.5.75.75 0 010-1.5z"/>
   </svg>
 </a>
-<a href="#" className="text-gray-300 hover:text-white transition-colors">
+ <a href="https://whatsapp.com/channel/0029Vb5gcb87YScyWqIfcX3b" target="_blank" className="text-gray-300 hover:text-white transition-colors">
+    <span className="sr-only">WhatsApp</span>
   <span className="sr-only">WhatsApp</span>
   <svg className="h-6 w-6" fill="currentColor" viewBox="0 0 24 24">
     <path d="M12 2a9.93 9.93 0 00-7.07 2.93A9.93 9.93 0 002 12c0 1.76.46 3.48 1.33 5.01L2 22l5.09-1.33A9.93 9.93 0 0012 22c2.65 0 5.15-1.03 7.07-2.93A9.93 9.93 0 0022 12c0-2.65-1.03-5.15-2.93-7.07A9.93 9.93 0 0012 2zm0 18a8.01 8.01 0 01-4.24-1.24l-.3-.18-3.02.79.8-2.95-.2-.31A8.01 8.01 0 014 12a8 8 0 0116 0c0 2.13-.83 4.14-2.34 5.66A7.97 7.97 0 0112 20zm3.71-5.29c-.2-.1-1.18-.58-1.36-.65-.18-.07-.31-.1-.44.1-.13.2-.5.65-.61.78-.11.13-.22.15-.42.05-.2-.1-.84-.31-1.6-.99-.59-.52-.99-1.16-1.11-1.36-.11-.2-.01-.31.08-.41.08-.08.2-.22.3-.33.1-.11.13-.2.2-.33.07-.13.03-.25-.02-.35-.05-.1-.44-1.06-.61-1.45-.16-.39-.33-.34-.44-.35h-.38c-.13 0-.35.05-.53.25-.18.2-.7.68-.7 1.65s.72 1.91.82 2.04c.1.13 1.41 2.15 3.42 3.01.48.21.85.34 1.14.44.48.15.92.13 1.27.08.39-.06 1.18-.48 1.35-.95.17-.47.17-.87.12-.95-.05-.08-.18-.13-.38-.23z"/>
   </svg>
 </a>
-<a href="#" className="text-gray-300 hover:text-white transition-colors">
-  <span className="sr-only">Facebook</span>
-  <svg className="h-6 w-6" fill="currentColor" viewBox="0 0 24 24">
-    <path d="M22 12a10 10 0 10-11.5 9.95v-7.05h-2.2V12h2.2V9.8c0-2.18 1.3-3.38 3.3-3.38.96 0 1.96.17 1.96.17v2.15h-1.1c-1.08 0-1.42.67-1.42 1.36V12h2.42l-.39 2.9h-2.03v7.05A10 10 0 0022 12z"/>
-  </svg>
+ <a href="https://x.com/gc_tech_pvt?s=11" target="_blank" className="text-gray-300 hover:text-white transition-colors">
+  <span className="sr-only">Twitter</span>
+ <svg className="h-6 w-6" fill="currentColor" viewBox="0 0 24 24">
+              <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z"/>
+            </svg>
 </a>
-<a href="#" className="text-gray-300 hover:text-white transition-colors">
+ <a href="https://youtube.com/@gctechtalenthub?si=fjgixVqkMWGKWnV8" target="_blank" className="text-gray-300 hover:text-white transition-colors">
   <span className="sr-only">YouTube</span>
   <svg className="h-6 w-6" fill="currentColor" viewBox="0 0 24 24">
     <path d="M23.5 6.2a2.9 2.9 0 00-2.05-2.05C19.5 3.5 12 3.5 12 3.5s-7.5 0-9.45.65A2.9 2.9 0 00.5 6.2 30.2 30.2 0 000 12a30.2 30.2 0 00.5 5.8 2.9 2.9 0 002.05 2.05C4.5 20.5 12 20.5 12 20.5s7.5 0 9.45-.65a2.9 2.9 0 002.05-2.05A30.2 30.2 0 0024 12a30.2 30.2 0 00-.5-5.8zM9.75 15.02V8.98L15.5 12l-5.75 3.02z"/>
--- src/components/sections/SocailMedia.jsx
+++ src/components/sections/SocailMedia.jsx
@@ -1,44 +1,46 @@
-import { hover, motion } from 'framer-motion';
-import { Briefcase, Facebook, FacebookIcon, Instagram, Linkedin, LinkedinIcon, Youtube } from 'lucide-react';
+import { motion } from 'framer-motion';
 
 function SocialMedia() {
   const socialLinks = [
     {
       name: 'LinkedIn',
-      url: '#',
-      icon:   <svg className="h-6 w-6" fill="currentColor" viewBox="0 0 24 24">
-                  <path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"/>
-                </svg>,
-                 color: 'text-gray-400 hover:text-gray-300',
-                
-     
-    },
-    {
-      name: 'Facebook',
-      url: '#',
+      url: 'https://www.linkedin.com/company/gc-technologies-pvt-ltd/',
       icon: <svg className="h-6 w-6" fill="currentColor" viewBox="0 0 24 24">
-    <path d="M22 12a10 10 0 10-11.5 9.95v-7.05h-2.2V12h2.2V9.8c0-2.18 1.3-3.38 3.3-3.38.96 0 1.96.17 1.96.17v2.15h-1.1c-1.08 0-1.42.67-1.42 1.36V12h2.42l-.39 2.9h-2.03v7.05A10 10 0 0022 12z"/>
-  </svg>,
+              <path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"/>
+            </svg>,
       color: 'text-gray-400 hover:text-gray-300',
-     
     },
     {
       name: 'Instagram',
-      url: '#',
+      url: 'https://www.instagram.com/gctechsoft?igsh=MTlmOGU4NTliaTByNA%3D%3D&utm_source=qr',
       icon: <svg className="h-6 w-6" fill="currentColor" viewBox="0 0 24 24">
-    <path d="M7.75 2h8.5A5.75 5.75 0 0122 7.75v8.5A5.75 5.75 0 0116.25 22h-8.5A5.75 5.75 0 012 16.25v-8.5A5.75 5.75 0 017.75 2zm0 1.5A4.25 4.25 0 003.5 7.75v8.5A4.25 4.25 0 007.75 20.5h8.5a4.25 4.25 0 004.25-4.25v-8.5A4.25 4.25 0 0016.25 3.5h-8.5zM12 7a5 5 0 110 10 5 5 0 010-10zm0 1.5a3.5 3.5 0 100 7 3.5 3.5 0 000-7zm5.25-.75a.75.75 0 110 1.5.75.75 0 010-1.5z"/>
-  </svg>,
+              <path d="M7.75 2h8.5A5.75 5.75 0 0122 7.75v8.5A5.75 5.75 0 0116.25 22h-8.5A5.75 5.75 0 012 16.25v-8.5A5.75 5.75 0 017.75 2zm0 1.5A4.25 4.25 0 003.5 7.75v8.5A4.25 4.25 0 007.75 20.5h8.5a4.25 4.25 0 004.25-4.25v-8.5A4.25 4.25 0 0016.25 3.5h-8.5zM12 7a5 5 0 110 10 5 5 0 010-10zm0 1.5a3.5 3.5 0 100 7 3.5 3.5 0 000-7zm5.25-.75a.75.75 0 110 1.5.75.75 0 010-1.5z"/>
+            </svg>,
       color: 'text-gray-400 hover:text-gray-300',
-     
     },
     {
       name: 'YouTube',
-      url: '#',
-      icon:  <svg className="h-6 w-6" fill="currentColor" viewBox="0 0 24 24">
-    <path d="M23.5 6.2a2.9 2.9 0 00-2.05-2.05C19.5 3.5 12 3.5 12 3.5s-7.5 0-9.45.65A2.9 2.9 0 00.5 6.2 30.2 30.2 0 000 12a30.2 30.2 0 00.5 5.8 2.9 2.9 0 002.05 2.05C4.5 20.5 12 20.5 12 20.5s7.5 0 9.45-.65a2.9 2.9 0 002.05-2.05A30.2 30.2 0 0024 12a30.2 30.2 0 00-.5-5.8zM9.75 15.02V8.98L15.5 12l-5.75 3.02z"/>
-  </svg>,
+      url: 'https://youtube.com/@gctechtalenthub?si=fjgixVqkMWGKWnV8',
+      icon: <svg className="h-6 w-6" fill="currentColor" viewBox="0 0 24 24">
+              <path d="M23.5 6.2a2.9 2.9 0 00-2.05-2.05C19.5 3.5 12 3.5 12 3.5s-7.5 0-9.45.65A2.9 2.9 0 00.5 6.2 30.2 30.2 0 000 12a30.2 30.2 0 00.5 5.8 2.9 2.9 0 002.05 2.05C4.5 20.5 12 20.5 12 20.5s7.5 0 9.45-.65a2.9 2.9 0 002.05-2.05A30.2 30.2 0 0024 12a30.2 30.2 0 00-.5-5.8zM9.75 15.02V8.98L15.5 12l-5.75 3.02z"/>
+            </svg>,
       color: 'text-gray-400 hover:text-gray-300',
-     
+    },
+    {
+      name: 'Twitter',
+      url: 'https://x.com/gc_tech_pvt?s=11',
+      icon: <svg className="h-6 w-6" fill="currentColor" viewBox="0 0 24 24">
+              <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z"/>
+            </svg>,
+      color: 'text-gray-400 hover:text-gray-300',
+    },
+    {
+      name: 'WhatsApp',
+      url: 'https://whatsapp.com/channel/0029Vb5gcb87YScyWqIfcX3b',
+      icon: <svg className="h-6 w-6" fill="currentColor" viewBox="0 0 24 24">
+              <path d="M17.472 14.382c-.297-.149-1.758-.867-2.03-.967-.273-.099-.471-.148-.67.15-.197.297-.767.966-.94 1.164-.173.199-.347.223-.644.075-.297-.15-1.255-.463-2.39-1.475-.883-.788-1.48-1.761-1.653-2.059-.173-.297-.018-.458.13-.606.134-.133.298-.347.446-.52.149-.174.198-.298.298-.497.099-.198.05-.371-.025-.52-.075-.149-.669-1.612-.916-2.207-.242-.579-.487-.5-.669-.51-.173-.008-.371-.01-.57-.01-.198 0-.52.074-.792.372-.272.297-1.04 1.016-1.04 2.479 0 1.462 1.065 2.875 1.213 3.074.149.198 2.096 3.2 5.077 4.487.709.306 1.262.489 1.694.625.712.227 1.36.195 1.871.118.571-.085 1.758-.719 2.006-1.413.248-.694.248-1.289.173-1.413-.074-.124-.272-.198-.57-.347m-5.421 7.403h-.004a9.87 9.87 0 01-5.031-1.378l-.361-.214-3.741.982.998-3.648-.235-.374a9.86 9.86 0 01-1.51-5.26c.001-5.45 4.436-9.884 9.888-9.884 2.64 0 5.122 1.03 6.988 2.898a9.825 9.825 0 012.893 6.994c-.003 5.45-4.437 9.884-9.885 9.884m8.413-18.297A11.815 11.815 0 0012.05 0C5.495 0 .16 5.335.157 11.892c0 2.096.547 4.142 1.588 5.945L.057 24l6.305-1.654a11.882 11.882 0 005.683 1.448h.005c6.554 0 11.89-5.335 11.893-11.893-.001-3.189-1.262-6.209-3.553-8.5"/>
+            </svg>,
+      color: 'text-gray-400 hover:text-gray-300',
     }
   ];
 
@@ -187,10 +189,10 @@ function SocialMedia() {
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           transition={{ delay: 1.6 }}
-          className="relative   text-xs font-semibold text-transparent bg-gradient-to-b from-white to-black bg-clip-text -rotate-90 origin-center whitespace-nowrap mb-8 overflow-hidden"
+          className="relative text-xs font-semibold text-transparent bg-gradient-to-b from-white to-black bg-clip-text -rotate-90 origin-center whitespace-nowrap mb-8 overflow-hidden"
         >
-          <span className=""> Follow Us</span>
-          <span className="absolute inset-0 bg-gradient-to-r from-transparent via-white/70 to-transparent animate-shine " />
+          <span className="">Follow Us</span>
+          <span className="absolute inset-0 bg-gradient-to-r from-transparent via-white/70 to-transparent animate-shine" />
         </motion.div>
       </motion.div>
     </>
